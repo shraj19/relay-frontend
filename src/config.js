fa365f97599298bb63fbc404c83283aa57686d5c
Shashank-raj1907 <shashankraj2116193@gmail.com>
2026-08-25 20:14:18 +0530

    chore: update backend URL to Render deployment

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-export const API_BASE_URL = "https://chat-production-22ce.up.railway.app"
-export const WS_BASE_URL = "wss://chat-production-22ce.up.railway.app";
+export const API_BASE_URL = "https://chat-vpd5.onrender.com"
+export const WS_BASE_URL = "wss://chat-vpd5.onrender.com"
 
 // export const API_BASE_URL = "http://localhost:8080"
 // export const WS_BASE_URL = "ws://localhost:8080"
